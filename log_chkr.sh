86396e333cb67560450a0099f3b43994d9a96a55
Cyrus Malekani <78088083+cyrusmalekani@users.noreply.github.com>
2021-02-22 12:11:45 +0100

    Update log_chkr.sh

--- log_chkr.sh
+++ log_chkr.sh
@@ -4,13 +4,13 @@ rad=$(cat /tmp/log/labb2.log | wc -l)
 
 if [ $rad -gt 9 ]
 then
-	echo ''
-	echo '##########################################################'
+    echo ''
+    echo '##########################################################'
     echo '###    /tmp/log/labb2.log HAS REACHED 10 LINES         ###'
     echo '### APPENDING /tmp/log/labb2.log TO /tmp/log/labb2.bak ###'
-    echo '###          Cleaning up /tmp/log/lab2.log !           ###'
+    echo '###          Cleaning up /tmp/log/labb2.log !          ###'
     echo '##########################################################'
-	echo ''
+    echo ''
 
     cat /tmp/log/labb2.log >> /tmp/log/labb2.bak
     truncate -s 0 /tmp/log/labb2.log
